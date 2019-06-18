2cbe023139fecc1a2b205d6515ab31a4f9624f5d
zaracky <l.esparon@rt-iut.re>
2019-06-18 13:41:24 -0700

    Ajout de commentaire sur la fonction menu et client

--- script.py
+++ script.py
@@ -1,14 +1,17 @@
 import sys, os
 import smtplib
 
+#Fonction Menu
 def menu():
 	os.system('clear')
-	print("Bienvenue à travers mon script d'automatisation des clients VPN \n ")
+#On affiche ici les options possible à l'éxecution du programme
+	print("Bienvenue à travers mon script d'automatisation des clients OPENVPN \n ")
 	print("\033[31m/_\ Attention ce dernier est à exécuter sur une machine GNU/Linux \n\033[0m") 
 	print("Voici les differentes options:")
 	print(" 1.Créer un fichier de configuration pour un client \n 2 Créer le fichier client sur la machine local \n 3.Quitter")
 	print(" \n Votre choix:")
 	choice = input(" >>")
+#Une fonction à exécuter en fonction de chaque choix
 	if choice=="1":
 		client()
 	elif choice=="2":
@@ -19,9 +22,12 @@ def menu():
 		menu()
 
 	return
+#Fin fonction menu
 
 
+#FOnction permettant la création de la clé client
 def client():
+#Des informations sont necessaires afin de personnaliser les clés
 	print("Merci de completer les informations suivantes afin de démarrer le script ")
 	print(" \n Entrer le nom du client:")
 	nom = input(" >>")
@@ -31,30 +37,35 @@ def client():
 	port = input(" >>")
 	print(" \n Le protocol utilisé est UDP ou TCP:")
 	protocol = input(" >>")
+#On affiche le résumé des données entrées afin que l'utilisateur valide en toute conscience
 	print("\nLes informations sont les suivantes:\nnom:",nom, "\nIP:",ip,"\nPort:",port,"\nProtocol:",protocol)
 	print(" \n Etes vous sur?(y/n)")
 	choice = input(" >>")
-
+#Si le choix est yes on commence la création de la clé ovpn
 	if choice=="y" or "Y":
 		print("nom:",nom,ip,port,protocol)
 
+
+#Si le choix est no  l'utilisateur est invité a entrer à nouveau les informations		
 	elif choice=="n" or "N":
 		print("Renseignez à nouveau les informations du client")
 		client()
+#Sinon on fait à nouveau appel a la fonction client
 	else:
 		client()
-
+#Maintenant que le fichier est créer on propose à l'utilisateur de l'envoyer par mail à l'utilisateur
 	print("Envoyer le fichier par mail à l'utilisateur?(y/n)")
 	reponse = input(" >>")
+#Si oui on fait appel à la fonction mail correspondant à l'OS Client
 	if reponse=="y" or "Y" :
 		print("nom:",nom)
-	elif reponse=="n" or "N" :
-		print("Renseignez à nouveau les informations du client")
-	else:
-		client()
 
+#Sinon on indique uniquement le chemin d'accès vers le fichier ovpn
+	else :
+		print("Le fichier est disponible à l'emplacement suivant:")
 
 	return
+#Fin de la fonction client
 
 
 def installation():
